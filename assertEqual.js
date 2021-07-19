6ec59723d595e350cfe110c53e3420bf5aa0cff0
Tzheng456 <Tzheng456@gmail.com>
2021-07-19 21:24:07 +0000

    Implemented the assertEqual function, and added support for emoji's to improve visual fidelity

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+let emoji = require('node-emoji');
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let passedEmoji = emoji.emojify(':white_check_mark:');
+  let failedEmoji = emoji.emojify(':octagonal_sign:');
+  return (actual === expected) ? console.log(passedEmoji + passedEmoji + passedEmoji + " Assertion Passed: " + actual + " === " + expected) :
+    console.log(failedEmoji + failedEmoji + failedEmoji + " Assertion Failed: " + actual + " === " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("test", "test");
+assertEqual("test", "nottest");
+assertEqual(5, 5);
+assertEqual(5, 6);
