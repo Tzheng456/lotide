9a7a97fa434f34d370c452df9aa47dfd9bb73f2a
Tzheng456 <Tzheng456@gmail.com>
2021-07-25 22:27:26 +0000

    added recursive functionality to eqObjects, can now handle nested Objects

--- eqObjects.js
+++ eqObjects.js
@@ -1,53 +1,54 @@
-const assertEqual = function(actual, expected) {
-  return (actual === expected) ? console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`) : 
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
+const eqObjects = (actual, expected) => {
+  let result = false;
+  //if the number of keys is the same
+  if (Object.keys(actual).length === Object.keys(expected).length) {
+    // loop through the keys of the actual object
+    for (const keyA in actual) {
+      // only proceed if keyA exists in both actual and expected (not undefined)
+      if (expected[keyA] !== undefined) {
+        // check if both key in actual object and key in expected are objects (DeMorgans Law: (!p ^ !q) === !(p v q) )
+        if (!(checkIfObject(actual[keyA]) || checkIfObject(expected[keyA]))) {
+          //not both actual[keyA] and expected[keyA] are objects, compare primitives
+          if (actual[keyA] === expected[keyA]) {
+            result = true;
+          }
+        } else {
+          // if both keys are objects, do recursive call
+          result = eqObjects(actual[keyA], expected[keyA]);
+          // if the recursive call returns false, should break out of loop and return result (false)
+          if (!result) {
+            return result;
+          }
+        }
+      } else {
+        // a key in actual is undefined in expected; return false
         return false;
       }
     }
-    return true;
   } else {
+    // objects are of different length; return false
     return false;
   }
-}
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  let result = false;
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (let key in object1) {
-      if(Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        result = eqArrays(object1[key], object2[key]);
-      } else {
-        if(object1[key] === object2[key]) {
-          result = true;
-        } else {
-          result = false;
-        }
-      }
-    }
-  } else {
-    return false;
-  }
+  // final result
   return result;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+// FUNCTION: checks if an element is an object
+const checkIfObject = (arg) => {
+  if (typeof arg === "object" && typeof arg !== null && !Array.isArray(arg)) {
+    return true;
+  }
+  return false;
+};
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+// let objA = { a: 1, b: 2, c: { d: 3, e: 4, f: { g: 123 } } };
+// let objB = { a: 1, c: { f: { g: 123 }, d: 3, e: 4 }, b: 2 };
+// let objC = { a: 1, b: 2, c: { d: 3, e: 4 }, f: 0 };
+// let objD = { a: 1, b: 2, c: { d: 3, z: 4 } };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+// console.log("test1:", eqObjects(objA, objB)); // => true
+// console.log("test2:", eqObjects(objA, objC)); // => false, length of keys diff
+// console.log("test3:", eqObjects(objA, objD)); // => false, undefined keyA in expected
+// console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+// console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
