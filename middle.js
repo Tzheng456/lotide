b39597509fbacc33bbab5545f76e72656a5d5ef7
Tzheng456 <Tzheng456@gmail.com>
2021-07-26 19:31:32 +0000

    refactored middle, added test for middle

--- middle.js
+++ middle.js
@@ -1,22 +1,22 @@
-const eqArrays = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+// const eqArrays = function (arr1, arr2) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
+// const assertArraysEqual = function (arr1, arr2) {
+//   if (eqArrays(arr1, arr2)) {
+//     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+//   }
+// };
 
-const middle = function(array) {
-  let midIdx = Math.floor(((array.length -1) / 2));
+const middle = function (array) {
+  let midIdx = Math.floor((array.length - 1) / 2);
   let output = [];
   if (array && array.length <= 2) {
     return [];
@@ -28,19 +28,11 @@ const middle = function(array) {
     output.push(array[midIdx], array[midIdx + 1]);
     return output;
   }
+};
 
-}
-
-const even = function(array) {
+const even = function (array) {
   return array.length % 2 === 0;
-}
+};
 // one or two elements -> []
-console.log(assertArraysEqual(middle([1]), [])) // => []
-console.log(assertArraysEqual(middle([1, 2]), [])) // => []
-// odd number of elements
-console.log(assertArraysEqual(middle([1, 2, 3]), [2])) // => [2]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])) // => [3]
-// even number of elements
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])) // => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])) // => [3, 4]
 
+module.exports = middle;
